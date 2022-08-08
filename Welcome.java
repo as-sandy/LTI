9bd137e078f413cb53e7e02319f0457b1e4d7b6f
as-sandy <110518976+as-sandy@users.noreply.github.com>
2022-08-08 13:29:27 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,6 @@
   public class Welcome {
   public static void main(String[] args) {
      string name = "Shree";
-     System.out.println("Happy Brthday Shree - With love dhiya"+namr;
+     System.out.println("Happy Brthday Shree - With love dhiya"+name);
   }
   }
